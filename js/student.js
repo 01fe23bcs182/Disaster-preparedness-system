137bd903d755c95eabad7b3d2553509c076e9ba2
01fe23bcs182 <01fe23bcs182@kletech.ac.in>
2025-09-18 00:07:42 +0530

    Update student.js

--- js/student.js
+++ js/student.js
@@ -1,31 +1,7 @@
-const logoutBtn = document.getElementById('logout');
 const imSafeBtn = document.getElementById('im-safe');
 
-logoutBtn.addEventListener('click', () => {
-  auth.signOut().then(() => {
-    window.location.href = '../index.html';
-  });
-});
-
-// Respond to drill
-if(imSafeBtn){
 imSafeBtn.addEventListener('click', () => {
-  const user = auth.currentUser;
-  if(user){
-    db.collection('drills').doc('current').set({
-      [user.uid]: 'safe'
-    }, {merge: true});
-    alert('Your status has been recorded!');
-  }
-});
-}
-
-// Listen for drills
-db.collection('drills').doc('current')
-.onSnapshot((doc) => {
-  if(doc.exists){
-    console.log('Drill active');
-    // You can add popup/notification
-  }
+  alert("Your 'I'm Safe' status has been recorded (simulation mode).");
 });
 
+// You can simulate live drill updates if needed
