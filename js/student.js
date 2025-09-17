a4bceff1830bc1c5347f9a402d87804e83fde6d3
01fe23bcs182 <01fe23bcs182@kletech.ac.in>
2025-09-17 23:55:23 +0530

    Update student.js

--- js/student.js
+++ js/student.js
@@ -1,1 +1,31 @@
+const logoutBtn = document.getElementById('logout');
+const imSafeBtn = document.getElementById('im-safe');
+
+logoutBtn.addEventListener('click', () => {
+  auth.signOut().then(() => {
+    window.location.href = '../index.html';
+  });
+});
+
+// Respond to drill
+if(imSafeBtn){
+imSafeBtn.addEventListener('click', () => {
+  const user = auth.currentUser;
+  if(user){
+    db.collection('drills').doc('current').set({
+      [user.uid]: 'safe'
+    }, {merge: true});
+    alert('Your status has been recorded!');
+  }
+});
+}
+
+// Listen for drills
+db.collection('drills').doc('current')
+.onSnapshot((doc) => {
+  if(doc.exists){
+    console.log('Drill active');
+    // You can add popup/notification
+  }
+});
 
