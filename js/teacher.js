d505683414d017e175f52d9b1bcf5dea80c2dace
01fe23bcs182 <01fe23bcs182@kletech.ac.in>
2025-09-17 23:55:46 +0530

    Update teacher.js

--- js/teacher.js
+++ js/teacher.js
@@ -1,1 +1,30 @@
+const logoutBtn = document.getElementById('logout');
+const startDrillBtn = document.getElementById('start-drill');
+const studentStatusList = document.getElementById('student-status');
 
+logoutBtn.addEventListener('click', () => {
+  auth.signOut().then(() => {
+    window.location.href = '../index.html';
+  });
+});
+
+startDrillBtn.addEventListener('click', () => {
+  db.collection('drills').doc('current').set({active: true});
+  alert('Drill started!');
+});
+
+// Show student status
+db.collection('drills').doc('current')
+.onSnapshot((doc) => {
+  if(doc.exists){
+    studentStatusList.innerHTML = '';
+    const data = doc.data();
+    for(const uid in data){
+      if(uid !== 'active'){
+        const li = document.createElement('li');
+        li.textContent = uid + ': ' + data[uid];
+        studentStatusList.appendChild(li);
+      }
+    }
+  }
+});
