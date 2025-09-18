22575e6bfc26801f7af6866ed46d8702a8a003f1
01fe23bcs182 <01fe23bcs182@kletech.ac.in>
2025-09-18 10:54:17 +0530

    Delete js directory

--- js/student.js
+++
@@ -1,7 +0,0 @@
-const imSafeBtn = document.getElementById('im-safe');
-
-imSafeBtn.addEventListener('click', () => {
-  alert("Your 'I'm Safe' status has been recorded (simulation mode).");
-});
-
-// You can simulate live drill updates if needed
